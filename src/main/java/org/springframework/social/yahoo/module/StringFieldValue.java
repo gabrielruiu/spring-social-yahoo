44f2376344877999c4b699ba395e9ec91cab20b5
Ruiu Gabriel Mihai <gabriel.ruiu@mail.com>
2014-11-28 14:13:03 +0200

    add StringFieldValue

---
+++ src/main/java/org/springframework/social/yahoo/module/StringFieldValue.java
@@ -0,0 +1,17 @@
+package org.springframework.social.yahoo.module;
+
+/**
+ * Ruiu Gabriel Mihai (gabriel.ruiu@1and1.ro)
+ */
+public class StringFieldValue extends FieldValue {
+
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
