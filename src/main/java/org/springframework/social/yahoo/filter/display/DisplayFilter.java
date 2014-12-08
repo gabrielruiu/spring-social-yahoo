d66cf832bfff6c60b269810403afd879fdf5b3e3
Ruiu Gabriel Mihai <gabriel.ruiu@mail.com>
2014-12-08 16:39:19 +0200

    implement basic display filter

--- src/main/java/org/springframework/social/yahoo/filter/display/DisplayFilter.java
+++ src/main/java/org/springframework/social/yahoo/filter/display/DisplayFilter.java
@@ -1,7 +1,32 @@
 package org.springframework.social.yahoo.filter.display;
 
+import org.springframework.social.yahoo.module.FieldType;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Ruiu Gabriel Mihai (gabriel.ruiu@1and1.ro)
  */
 public class DisplayFilter {
+
+    private List<String> fields = new ArrayList<>();
+
+    public DisplayFilter withField(FieldType field) {
+        fields.add(field.name().toLowerCase());
+        return this;
+    }
+
+    public String build() {
+        StringBuilder sb = new StringBuilder();
+        if (fields.size() > 0) {
+            for (String field : fields) {
+                sb.append(field);
+                if (fields.size() > 1 && !field.equals(fields.get(fields.size() - 1))) {
+                    sb.append(",");
+                }
+            }
+        }
+        return sb.toString();
+    }
 }
