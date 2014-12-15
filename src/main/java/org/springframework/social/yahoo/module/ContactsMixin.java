2880a2a6d94c827c1cbf8ab367aafafa718724d4
Gabriel Ruiu <gabriel.ruiu@mail.com>
2014-12-15 23:28:46 +0200

    ignore serializing field values retrieved through getters from Contact

--- src/main/java/org/springframework/social/yahoo/module/ContactsMixin.java
+++ src/main/java/org/springframework/social/yahoo/module/ContactsMixin.java
@@ -16,6 +16,7 @@
 
 package org.springframework.social.yahoo.module;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
@@ -43,4 +44,20 @@ public abstract class ContactsMixin extends YahooObjectMixin {
     @JsonProperty("updated") Date update;
 
     @JsonProperty("contact") List<Contact> contact;
+
+    @JsonIgnore abstract String getEmail();
+    @JsonIgnore abstract String getGuid();
+    @JsonIgnore abstract String getYahooId();
+    @JsonIgnore abstract String getOtherId();
+    @JsonIgnore abstract String getCompany();
+    @JsonIgnore abstract String getNickName();
+    @JsonIgnore abstract String getPhone();
+    @JsonIgnore abstract String getJobTitle();
+    @JsonIgnore abstract String getNotes();
+    @JsonIgnore abstract String getLink();
+    @JsonIgnore abstract String getCustom();
+    @JsonIgnore abstract DateFieldValue getBirthday();
+    @JsonIgnore abstract DateFieldValue getAnniversary();
+    @JsonIgnore abstract NameFieldValue getName();
+    @JsonIgnore abstract AddressFieldValue getAddress();
 }
